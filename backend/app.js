2715dd763d4f36563f06ceb2085e1c7a8880855b
Vantol Bennett (titan) <vvantol2000@icloud.com>
2021-02-28 23:44:14 -0500

    Update app.js
    Modification of the url endpoints

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/school', schoolWorkRouter);
+app.use('/', usersRouter);
+app.use('/', schoolWorkRouter);
 
 module.exports = app;
