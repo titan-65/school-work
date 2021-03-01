623bcc79ff29cc6e1d8fb67a7f6c493782a504e1
Vantol Bennett (titan) <vvantol2000@icloud.com>
2021-02-28 23:34:06 -0500

    Create school.controoller.js
    School controller created for work routes

---
+++ backend/controllers/school.controoller.js
@@ -0,0 +1,10 @@
+const list = (req, res, next) => {
+
+}
+const getWork = (req, res, next) => {
+    const id = req.params.workWeekId
+
+    // const workWeek =
+}
+
+module.exports = { list, getWork }
