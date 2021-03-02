7a72ca04c6e02b3e22fa30ef956948721c97f4b5
Vantol Bennett (titan) <vvantol2000@icloud.com>
2021-03-02 00:07:11 -0500

    Route Handler SchoolWork
    Created all route handlers for all school work endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -7,14 +7,21 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const schoolWorkRouter = require('./routes/school')
 
-var app = express();
+const app = express();
 
+/**  
+    Middleware
+*/
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+/**
+    Routes
+ */
 app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/', schoolWorkRouter);
--- backend/routes/school.js
+++ backend/routes/school.js
@@ -1,8 +1,124 @@
 const express = require('express');
 const router = express.Router();
-const { list, getWork } = require('../controllers/school.controoller')
+// const { list, getWork } = require('../controllers/school.controoller')
 
-router.get('/api/school-work', list)
-router.get('/api/school-work/:workWeekId', getWork)
+const schoolWorkArray = [{
+    id: 23,
+    title: 'SchoolWork',
+    author: 'V.Bennett',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sd safsaf sdffds onapif asdjpoasjpojsapojfpo fsoajfpojp[oaj',
+    grade: 8,
+    school: 'Eltham High School',
+    rating: 3.5,
+    images: [
+        {}
+    ],
+    comments: [{
+            id: 2,
+            comment: 'Excellent use of '
+        },
+        {
+            id: 3,
+            comment: 'How do we get the work from here?'
+        }
+    ]
+
+
+}, {
+    id: 24,
+    title: 'SchoolWork',
+    author: 'V.Bennett',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,sd safsaf sdffds onapif asdjpoasjpojsapojfpo fsoajfpojp[oaj',
+    grade: 9,
+    school: 'Eltham High School',
+    rating: 4.5,
+    images: [
+        {}
+    ],
+    comments: [{
+            id: 2,
+            comment: 'Excellent use of '
+        },
+        {
+            id: 3,
+            comment: 'How do we get the work from here?'
+        }
+    ]
+
+
+}]
+
+const getAllWork = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: schoolWorkArray.length,
+        data: {
+            schoolWorkArray
+        }
+    })
+}
+
+const createWork = (req, res) => {
+    const schoolWork = []
+    const newSchoolWork = req.body
+    schoolWork.push(newSchoolWork)
+    return res.status(201).json({
+        status: 'success',
+        data: {
+            schoolWork
+        }
+    })
+
+}
+
+const getWork = (req, res) => {
+    console.log(req.params)
+    const schoolWorkId = req.params.id * 1
+    const schoolWork = schoolWorkArray.find(wrk => wrk.id === schoolWorkId)
+    res.status(200).json({
+        status: 'success',
+        data: {
+            schoolWork
+        }
+    })
+}
+
+const updateWork = (req, res) => {
+    const schoolWorkId = req.params.id * 1
+    const schoolWork = schoolWorkArray.find(wrk => wrk.id === schoolWorkId)
+    res.status(24).json({
+        status: 'success',
+        data: {
+            schoolWork
+        }
+    })
+}
+
+const deletedWork = (req, res) => {
+    const schoolWorkId = req.params.id * 1
+    const schoolWork = schoolWorkArray.find(wrk => wrk.id === schoolWorkId)
+    res.status(24).json({
+        status: 'success',
+        data: {
+            schoolWork
+        }
+    })
+}
+
+router
+    .route('/api/v1/school-work')
+    .get(getAllWork)
+    .post(createWork)
+
+router.route('/api/v1/school-work/:id')
+    .get(getWork)
+    .patch(updateWork)
+    .delete(deletedWork)
+    // router.get('/api/school-work', list)
+
+// router.get('/api/v1/school-work')
+// router.post('/api/v1/school-work')
+// router.get('/api/school-work/:workWeekId', getWork)
+// router.get('/api/v1/school-work/:id')
 
 module.exports = router
