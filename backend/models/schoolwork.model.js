acfebc63b65d2c06d0fc5d3c188f0b1fc0630e7d
Vantol Bennett (titan) <vvantol2000@icloud.com>
2021-03-02 00:08:15 -0500

    User routes

--- backend/models/schoolwork.model.js
+++ backend/models/schoolwork.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 const SchoolWork = new Schema({
-
+    title: { type: 'String', required: true },
+    author: { type: 'String', required: true },
 })
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
+
+const getAllUsers = () => {
+        res.status(500).json({
+            status: 'error',
+            messages: 'This route not yet created'
+        })
+    }
+    /* GET users listing. */
+router.route('/api/v1/users')
+    .get(getAllUsers)
+    .post(createUser)
+router.route('/api/v1/users/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser)
+
+
 router.get('/api/users', function(req, res, next) {
     res.send('respond with a resource');
 });
