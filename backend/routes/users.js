f342f701ba96dff4bef868bb7d6abb17d604a35b
Vantol Bennett (titan) <vvantol2000@icloud.com>
2021-03-02 00:13:14 -0500

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,12 +3,11 @@ const router = express.Router();
 
 
 const getAllUsers = () => {
-        res.status(500).json({
-            status: 'error',
-            messages: 'This route not yet created'
-        })
-    }
-    /* GET users listing. */
+    res.status(500).json({
+        status: 'error',
+        messages: 'This route not yet created'
+    })
+}
 router.route('/api/v1/users')
     .get(getAllUsers)
     .post(createUser)
@@ -17,9 +16,9 @@ router.route('/api/v1/users/:id')
     .patch(updateUser)
     .delete(deleteUser)
 
-
-router.get('/api/users', function(req, res, next) {
-    res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/api/users', function(req, res, next) {
+//     res.send('respond with a resource');
+// });
 
 module.exports = router;
