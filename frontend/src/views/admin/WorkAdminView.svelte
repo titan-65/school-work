59ec9d17b234f912cefc1c5eb48487f0f3887355
Vantol Bennett (titan) <vvantol2000@icloud.com>
2021-03-04 22:42:37 -0500

    Update WorkAdminView.svelte

--- frontend/src/views/admin/WorkAdminView.svelte
+++ frontend/src/views/admin/WorkAdminView.svelte
@@ -1,6 +1,7 @@
 <script>
     // import { user } from '../../stores.js'
     // let date = new Date().getTime()
+    import CardDetails from '../../components/features/admin/CardDetails.svelte'
     import {onMount} from 'svelte'
     import axios from 'axios'
 
@@ -9,7 +10,7 @@
     onMount(async () => {
         try {
             let response = await axios.get('http://localhost:3000/api/v1/school-work')
-            schoolWeekList = response.data
+            schoolWeekList = response.data.data.schoolWorks
             console.log(schoolWeekList)
         } catch (error) {
             console.error(error)
@@ -18,6 +19,20 @@
 </script>
 <main>
     <div class=" h-full w-full absolute">
-        
+        <section class="section">
+            <div class="container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Week of</th>
+                            <th>Published</th>
+                        </tr> 
+                    </thead>
+                    {#each schoolWeekList as schooWeek}
+                         <!-- content here -->
+                    {/each}
+                </table>
+            </div>
+        </section>
     </div>
 </main>
